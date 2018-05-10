5e431c0e22c78f1e071ec187e125fca4eb68c2f3
Rakesh reddy <rpd54@mail.umkc.edu>
2018-05-09 23:51:49 -0500

    ice7

---
+++ ICE7/MountainCar.py
@@ -0,0 +1,14 @@
+import gym
+env = gym.make('MountainCar-v0')
+observation = env.reset()
+print("observation=",observation)
+for t in range(100):
+    env.render()
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+    print("observation=",observation)
+    print("reward=",reward)
+    print("done=",done)
+    if done:
+        print("Episode finished after {} timesteps".format(t+1))
+        break
